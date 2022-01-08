855d3a85cfb5a894e4e9ae5e744d182bbd51fa9b
MO Nasser <39590845+MU-N@users.noreply.github.com>
2022-01-08 18:34:22 +0200

    adding api

--- Assets/__Project Files/Scripts/APIHelper.cs
+++ Assets/__Project Files/Scripts/APIHelper.cs
@@ -1,9 +1,15 @@
 using System.Net;
+using System.IO;
+using UnityEngine;
 
 public static class APIHelper 
 {
    public static Joke GetNewJoke()
     {
-        return new Joke();
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.chucknorris.io/jokes/random");
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jason = reader.ReadToEnd();
+        return JsonUtility.FromJson<Joke>(jason);
     }
 }
--- Assets/__Project Files/Scripts/JokeHandeler.cs
+++ Assets/__Project Files/Scripts/JokeHandeler.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class JokeHandeler : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMeshProUGUI;
+    
+    private void Start()
+    {
+        GetJoke();
+    }
+
     public void GetJoke()
     {
-
+        Joke j = APIHelper.GetNewJoke();
+        textMeshProUGUI.text = j.value;
     }
 }
